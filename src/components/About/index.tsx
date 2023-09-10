a244e6372e364c499fa99dbe98c8d6aa2c028cd1
jan.gren@protonmail.com <johndoe@example.com>
2023-09-10 18:18:31 +0200

    fix

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,7 +5,7 @@ const About = () => {
         <div id="about" className={styles.about_container}>
             <h2 className={styles.about_heading}>ABOUT ME</h2>
             <p className={styles.about_content}>
-                Passionate pront-end developer, I enjoy working on diverse
+                Passionate front-end developer, I enjoy working on diverse
                 projects using broad portfolio of technologies.
             </p>
         </div>
